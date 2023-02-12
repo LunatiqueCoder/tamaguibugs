d1bf65b1fb2eea2bf8de940715494c66474adb9e
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2023-02-12 11:35:35 +0200

    refactor

--- babel.config.js
+++ babel.config.js
@@ -4,12 +4,6 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
-      '@babel/plugin-transform-react-jsx',
-      {
-        runtime: 'automatic',
-      },
-    ],
-    [
       '@tamagui/babel-plugin',
       {
         components: ['tamagui'],
