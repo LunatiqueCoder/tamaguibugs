313755a0b02f59be78661bb943b363e749ef8f53
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2023-02-12 10:49:14 +0200

    Alter babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
+process.env.TAMAGUI_TARGET = 'native';
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      '@babel/plugin-transform-react-jsx',
+      {
+        runtime: 'automatic',
+      },
+    ],
+    [
+      '@tamagui/babel-plugin',
+      {
+        components: ['tamagui'],
+        config: './tamagui',
+      },
+    ],
+    // be sure to set TAMAGUI_TARGET
+    [
+      'transform-inline-environment-variables',
+      {
+        include: 'TAMAGUI_TARGET',
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
