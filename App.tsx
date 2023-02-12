6b9e6efe3369413c582f4f539398b0ceb4c611a4
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2023-02-12 11:34:55 +0200

    add Tamagui starter example

--- App.tsx
+++ App.tsx
@@ -1,118 +1,123 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import {useState} from 'react';
+import {ChevronDown, ChevronUp} from '@tamagui/lucide-icons';
+import {TamaguiProvider} from 'tamagui';
+import {useColorScheme} from 'react-native';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  Anchor,
+  Button,
+  H1,
+  Paragraph,
+  Separator,
+  Sheet,
+  XStack,
+  YStack,
   Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  Input,
+} from 'tamagui';
+import config from './tamagui';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+function SheetDemo() {
+  const [open, setOpen] = useState(false);
+  const [position, setPosition] = useState(0);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <>
+      <Button
+        aria-label={'toggle-sheet-button'}
+        size="$6"
+        icon={open ? ChevronDown : ChevronUp}
+        circular
+        onPress={() => setOpen(x => !x)}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <Sheet
+        modal
+        open={open}
+        onOpenChange={setOpen}
+        snapPoints={[80]}
+        position={position}
+        onPositionChange={setPosition}
+        dismissOnSnapToBottom>
+        <Sheet.Overlay />
+        <Sheet.Frame ai="center" jc="center" theme={'light'}>
+          <Sheet.Handle />
+          <Text>Username: </Text>
+
+          <Input size="$4" borderWidth={2} />
+
+          <Text>Password: </Text>
+
+          <Input
+            keyboardType="number-pad"
+            size="$4"
+            borderWidth={2}
+            theme={'light'}
+          />
+
+          <Button
+            size="$6"
+            aria-label={'close-sheet-button'}
+            // onPress={handleSubmit(onSubmit)}
+          >
+            Login
+          </Button>
+          <Button
+            size="$6"
+            circular
+            icon={ChevronDown}
+            aria-label={'close-sheet-button'}
+            onPress={() => {
+              setOpen(false);
+            }}
+          />
+        </Sheet.Frame>
+      </Sheet>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+function App() {
+  const theme = useColorScheme() || 'light';
+
+  return (
+    <TamaguiProvider config={config} disableInjectCSS defaultTheme={theme}>
+      <YStack f={1} jc="center" ai="center" p="$4" space>
+        <YStack space="$4" maw={600}>
+          <H1 ta="center" fontFamily={'$silkscreen'}>
+            Welcome to Tamagui.
+          </H1>
+          <Paragraph ta="center">
+            Here&apos;s a basic starter to show navigating from one screen to
+            another. This screen uses the same code on Next.js and React Native.
+          </Paragraph>
+
+          <Separator />
+          <Paragraph ta="center">
+            Made by{' '}
+            <Anchor
+              color="$color12"
+              href="https://twitter.com/natebirdman"
+              target="_blank">
+              @natebirdman
+            </Anchor>
+            ,{' '}
+            <Anchor
+              color="$color12"
+              href="https://github.com/tamagui/tamagui"
+              target="_blank"
+              rel="noreferrer">
+              give it a ⭐️
+            </Anchor>
+          </Paragraph>
+        </YStack>
+
+        <XStack>
+          <Button>Link to user</Button>
+        </XStack>
+
+        <SheetDemo />
+      </YStack>
+    </TamaguiProvider>
+  );
+}
 
 export default App;
